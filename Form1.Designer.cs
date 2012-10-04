50f5dba93c7e316562c19862d505a7055b827f27
devshark <ast.lim12@gmail.com>
2012-10-04 13:53:15 +0800

    Version 1 : basic functionality

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace InsertFromCSVGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(112, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open CSV file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(130, 12);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(119, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Location = new System.Drawing.Point(12, 41);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSQL.Size = new System.Drawing.Size(237, 274);
+            this.txtSQL.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 352);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Para sayo pareng Lawrence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtSQL;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace InsertFromCSVGenerator
+{
+    public partial class Form1 : Form
+    {
+        public String Filename = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV|*.csv", Title = "Find CSV file",CheckFileExists=true };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.Filename = ofd.FileName;
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (Filename == null)
+                MessageBox.Show("No file specified.");
+            String[] lines = File.ReadAllLines(this.Filename, Encoding.UTF8);
+            String main = String.Format("insert into {0}({1}) values", Path.GetFileNameWithoutExtension(this.Filename),lines[0]);
+            for (int i = 1; i < lines.Length; i++)
+            {
+                main += String.Format("({0}),", lines[i]);
+            }
+            main = main.Remove(main.Length - 1);
+            txtSQL.Text = main;
+        }
+
+    }
+}
